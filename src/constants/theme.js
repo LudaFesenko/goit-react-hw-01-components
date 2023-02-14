1ce644fb9cce138320cad311024d6f62ec3eac6a
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-02-14 17:08:43 +0200

    add theme

---
+++ src/constants/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    grayBasic: '#ccc',
+    grayTableRow: '#d3d3d3',
+    grayDark: '#686868',
+    primaryGrayColor: `rgb(135, 130, 130)`,
+    white: '#fff',
+  },
+  spacing: value => `${4 * value}px`,
+};
